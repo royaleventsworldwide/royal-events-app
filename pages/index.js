6e36b1b86620f9b04761331b2d124dac2368719e
royaleventsworldwide <royaleventsworldwide@gmail.com>
2025-11-18 07:17:53 -0500

    Add Home component for Royal Events booking app

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Royal Events Worldwide</h1>
+      <p>Welcome to the Royal Events booking + store app.</p>
+    </div>
+  );
+}
